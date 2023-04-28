00efa8a6e295d9c37abfe6895124fe0e4faa50b9
Ian <imonterroso-2018253@kinal.edu.gt>
2023-04-28 11:54:05 -0600

    Se modifico el lease

--- Almacenadora_Backend/src/lease/lease.controller.js
+++ Almacenadora_Backend/src/lease/lease.controller.js
@@ -2,6 +2,8 @@
 
 const Lease = require('./lease.model');
 const User = require('./user.model');
+const infoUser = ['name', 'surname'];
+const infoCellar = ['name','description', 'availability', 'price'];
 
 exports.createLease = async(req, res)=>{
     try{
@@ -33,10 +35,33 @@ exports.getLeases = async(req, res)=>{
         let { fact } = req.body;
         if(!fact) return res.status(400).send({message: 'Param fact is required'});
         let facts = await User.find({fact: fact})
-            .populate('user', )
+            .populate('user', infoUser)
+            .populate('cellar', infoCellar)
             .lean
+        if(facts.length === 0) return res.status(418).send({message: 'facts not found'});
+        return res.send({message: 'Leases: ', facts});
     }catch(err){
         console.error(err);
         return res.status(500).send({message: 'Error getting leases'});
+    }
+}
+
+exports.updateLease = async(req, res)=>{
+    try{
+        let userId = req.params.id;
+        let data = req.body;
+        if(userId != req.user.sub) return res.status(401).send({message: 'This cellar is already lease'})
+    }catch(err){
+        console.error(err);
+        return res.status(500).send({message: 'Error update a lease'});
+    }
+}
+
+exports.deleteLease = async(req, res)=>{
+    try{
+
+    }catch(err){
+        console.error(err);
+        return res.status(500).send({message: 'Error delete a lease'});
     }
 }
